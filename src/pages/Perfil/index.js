4b9040b8bb9ce03c0efb11fde5b1271c99a123a8
unknown <rafa_zanetti_c@hotmail.com>
2020-11-23 16:49:21 -0300

    projeto clone interface iFood

---
+++ src/pages/Perfil/index.js
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { Text } from 'react-native';
+
+import {
+  SafeAreaView,
+  Image,
+  View,
+  ButtonContainer,
+} from './styles';
+
+export default function Perfil() {
+  return (
+    <>
+      <StatusBar style="theme-dark" />
+      <SafeAreaView>
+        <View>
+          <Text>Perfil</Text>
+        </View>
+      </SafeAreaView>
+    </>
+  );
+}
